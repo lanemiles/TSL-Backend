58eb10f7345dca9d72cc1e55f785b60d37067407
Lane Miles <lanem1234@aol.com>
2015-04-21 13:52:02 -0700

    Add WaitingArticle to Admin

--- tsl_news/admin.py
+++ tsl_news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from tsl_news.models import Section, Author, Article
+from tsl_news.models import Section, Author, Article, WaitingArticle
 
 admin.site.register(Author)
 admin.site.register(Article)
 admin.site.register(Section)
+admin.site.register(WaitingArticle)
