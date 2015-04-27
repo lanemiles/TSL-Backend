ffbdcf99d4e5992aa4e9ad34b2bd04356ef63744
Lane Miles <lanem1234@aol.com>
2015-04-27 11:54:19 -0700

    Adding URL names

--- tsl_news/urls.py
+++ tsl_news/urls.py
@@ -5,11 +5,11 @@ from tsl_news import views
 
 # pattern matching the URLs
 urlpatterns = [
-    url(r'^articles/(?P<userID>[-\w]+)/(?P<articleID>\d+)', views.article, name='article_view'),
-    url(r'^sections/(?P<sectionName>.+)$', views.section),
-    url(r'^addArticle/$', views.add_article_url),
-    url(r'^featured/$', views.featured),
-    url(r'^users/(?P<userID>[-\w]+)$', views.user_favorite),
-    url(r'^addFavorite/(?P<userID>[-\w]+)/(?P<articleID>.+)$', views.add_favorite),
-    url(r'^removeFavorite/(?P<userID>[-\w]+)/(?P<articleID>.+)$', views.remove_favorite)
+    url(r'^articles/(?P<userID>[-\w]+)/(?P<articleID>\d+)', views.article, name='indiv_article_view_plus_favorite'),
+    url(r'^sections/(?P<sectionName>.+)$', views.section, name='section_list'),
+    url(r'^addArticle/$', views.add_article_url, name='add_article_form'),
+    url(r'^featured/$', views.featured, name='featured_list'),
+    url(r'^users/(?P<userID>[-\w]+)$', views.user_favorite, name='user_favorite_list'),
+    url(r'^addFavorite/(?P<userID>[-\w]+)/(?P<articleID>.+)$', views.add_favorite, name='user_add_favorite'),
+    url(r'^removeFavorite/(?P<userID>[-\w]+)/(?P<articleID>.+)$', views.remove_favorite, name='user_remove_favorite')
 ]
